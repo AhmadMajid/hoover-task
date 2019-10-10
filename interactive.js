a81bf06daf745715a3c3ef9d40d4cf54837796e9
Ahmad Majid <Ahmadmajid1994@gmail.com>
2019-10-10 18:58:34 +0100

    Trying to make the event listeners work

--- interactive.js
+++ interactive.js
@@ -1,13 +1,16 @@
 window.addEventListener('load', () => {
-  let roomDimensions = [5,5];
-  let hooverPosition = [1,2];
-  let patchDirt = [[1,0],[2,2],[2,3]];
+  let roomDimensions;
+  let hooverPosition;
+  let patchDirt;
   let instructions = 'NNESEESWNWW'
+  let lines;
+  let hoover;
   const hooverPositionX = document.querySelector('.hoover-position .x-coordinate')
   const hooverPositionY = document.querySelector('.hoover-position .y-coordinate')
   const dirt = document.querySelector('.dirt .patch-dirt')
   const coordinates = document.querySelector('.coordinate')
   const dirtyPatchesMessage = document.querySelector('.patch-dirt-mesesage')
+  const input = document.querySelector('input[type="file"]')
 
   let hoover = new Hoover(roomDimensions, hooverPosition, patchDirt, instructions)
 
@@ -21,5 +24,32 @@ window.addEventListener('load', () => {
   dirtyPatchesMessage.addEventListener('click', () => {
     dirt.textContent = hoover.counter
   })
+
+
+  input.addEventListener('change', (c) => {
+    const reader = new FileReader()
+    reader.onload = () => {
+      lines = reader.result.split('\n').map((line) => {
+        return line.replace(/ /g, "").split('')
+      })
+      lines.splice(-1,1)
+      roomDimensions = stringToInt(lines[0])
+      hooverPosition = stringToInt(lines[1])
+      patchDirt = [stringToInt(lines[2]), stringToInt(lines[3]), stringToInt(lines[4])]
+      hoover = new Hoover(roomDimensions, hooverPosition, patchDirt, instructions)
+      hoover.runHoover(hoover.instructions)
+    }
+    reader.readAsText(input.files[0])
+  }, false)
+
+  function stringToInt(array){
+    let newArray=[];
+    for(var i=0; i<array.length; i++) {
+      newArray.push(parseInt(array[i]))
+    }
+    return newArray
+  }
+
+
 
 })
